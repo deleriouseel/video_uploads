4f3ab8753054c8878c58a41fbcb3bd17ad61d5cb
deleriouseel <kristin@northcountrychapel.com>
2024-05-21 16:44:41 -0700

    description fix

--- upload_vimeo.py
+++ upload_vimeo.py
@@ -1,6 +1,7 @@
 import vimeo
 import os
 import get_wp_title
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,10 +18,12 @@ upload_name = get_wp_title.getName(os.getenv("WP_API_URL"))
 desktop = os.path.expanduser("~/Desktop")
 file_name = desktop + "/thurber.eric.mp4"
 tags = ["northcountrychapel", "ncc", "biblestudy"]
+regex = r'\b(?:[1-3]?\s?[a-zA-Z]+\s?[A-Za-z]*)\b'
+book_name = re.search(upload_name, regex)
 
 uri = client.upload(file_name, data={
   'name': upload_name,
-  'description': 'testing the upload',
+  'description': 'Join us as we study through the book of '+ str(book_name),
   'privacy': {
     'view': 'nobody' # prod = anybody
   },
